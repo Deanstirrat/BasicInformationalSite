b0ae1b441c157bcddee918061736de87d5cfd44d
Dean Stirrat <deanstirrat@gmail.com>
2023-08-24 13:52:50 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+http.createServer((req, res) => {
+    var q = url.parse(req.url)
+
+    let filename = (q.pathname == '/') ? 'index.html' :
+                   (q.pathname == '/about') ? 'about.html' :
+                   (q.pathname == '/contact-me') ? 'contact-me.html' :
+                   '404.html'
+
+    fs.readFile(filename, (err, data) => {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end()
+    })
+}).listen(8080)
